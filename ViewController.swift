dd07a05abbb87a52419c591e54e7efd33591592d
AlenaziHazal <roomyham@MacBook-Pro.local>
2022-09-07 16:48:03 +0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadFromFirebase
+//
+//  Created by AlenaziHazal on 11/02/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
